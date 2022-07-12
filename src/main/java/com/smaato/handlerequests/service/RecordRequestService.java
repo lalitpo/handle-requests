95a3c61918555efb0298c36893c8cf4bf0ecb8ed
Lalit Poddar <lalit-kumar.poddar@uni-konstanz.de>
2022-07-12 23:45:54 +0200

    application and log properties

--- src/main/java/com/smaato/handlerequests/service/RecordRequestService.java
+++ src/main/java/com/smaato/handlerequests/service/RecordRequestService.java
@@ -5,12 +5,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.smaato.handlerequests.constants.HandleRequestsConstant;
 
@@ -28,8 +31,13 @@ public class RecordRequestService {
     @Value("${kafka.enabled}")
     private boolean kafkaFlow;
 
+    @Value("${logFile}")
+    private String logFileName;
+
     @Autowired
     private ExternalCallService externalCallService;
+
+    Set<Integer> idMinRec = new HashSet<>();
 
     /*
      * <<<<<< Extension 2 :
@@ -38,26 +46,20 @@ public class RecordRequestService {
     @Transactional
     public int recordRequests(int id, boolean returnReqCount) throws FileNotFoundException, IOException {
 
-        List<String> logRecord = readLogFile();
-
-        if (!logRecord.contains(String.valueOf(id))) {
+        idMinRec.add(id);
 
             applog.info(String.valueOf(id));
 
             if (returnReqCount) {
 
                 if (kafkaFlow) {
-                    publishToKafka(logRecord.size() + 1);
+                    publishToKafka(idMinRec.size());
                 }
-                return logRecord.size() + 1; // as current request ID has also been added into the log file.
+                return idMinRec.size(); // as current request ID has also been added into the log file.
 
             }
 
-        }
-        if (kafkaFlow) {
-            publishToKafka(logRecord.size());
-        }
-        return logRecord.size();
+        return idMinRec.size();
     }
 
     /*
@@ -70,21 +72,6 @@ public class RecordRequestService {
         externalCallService.callThirdParty(HandleRequestsConstant.CALLING_HOST + kafkaPort.concat(kafkaUrl),
                 trafficCount);
 
-    }
-
-    private List<String> readLogFile() {
-
-        List<String> loggedId = new ArrayList<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader("manage-requests-idTraffic.log"))) {
-            String strLine;
-            while ((strLine = br.readLine()) != null) {
-                loggedId.add(strLine);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return loggedId;
     }
 
 }
