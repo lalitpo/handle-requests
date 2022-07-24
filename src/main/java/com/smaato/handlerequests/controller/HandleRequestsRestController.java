064fa3162b52489d96524dbdfc6670d604fc64c3
Lalit Poddar <lalit-kumar.poddar@uni-konstanz.de>
2022-07-24 14:58:19 +0200

    remove unused import

--- src/main/java/com/smaato/handlerequests/controller/HandleRequestsRestController.java
+++ src/main/java/com/smaato/handlerequests/controller/HandleRequestsRestController.java
@@ -9,7 +9,6 @@ import com.smaato.handlerequests.interfaces.HandleRequestsInterface;
 import com.smaato.handlerequests.service.RecordRequestService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
